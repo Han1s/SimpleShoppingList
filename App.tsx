6ddc9e686f73fad28dbde55205c6faca752cf93d
Jan Ranostaj <HonzaRanostaj@gmail.com>
2024-04-06 19:09:45 +0200

    add list

--- App.tsx
+++ App.tsx
@@ -1,14 +1,49 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  FlatList,
+} from "react-native";
 import IconButton from "./components/UI/IconButton";
+import { useState } from "react";
+
+const MOCK_DATA = ["apple", "oranges"];
 
 export default function App() {
+  const [list, setList] = useState<Array<string>>(MOCK_DATA);
+  const [input, setInput] = useState("");
+
+  const addItemToList = () => {
+    if (!input) return;
+
+    setList((prevList) => [input, ...prevList]);
+    setInput("");
+  };
+
   return (
     <View style={styles.appContainer}>
       <StatusBar style="auto" />
       <View style={styles.inputContainer}>
-        <TextInput placeholder="I want to buy..." onChangeText={() => {}} />
-        <IconButton icon="add" size={24} color={"pink"} onPress={() => {}} />
+        <TextInput
+          placeholder="To buy..."
+          value={input}
+          onChangeText={(text) => setInput(text)}
+        />
+        <IconButton
+          icon="add"
+          size={24}
+          color={"pink"}
+          onPress={addItemToList}
+        />
+      </View>
+      <View style={styles.listContainer}>
+        <FlatList
+          data={list}
+          renderItem={({ item }) => <Text style={styles.listItem}>{item}</Text>}
+        />
       </View>
     </View>
   );
@@ -28,5 +63,14 @@ const styles = StyleSheet.create({
     marginBottom: 24,
     borderBottomWidth: 1,
     borderBottomColor: "grey",
+  },
+  listContainer: {
+    flex: 1,
+    paddingTop: 22,
+  },
+  listItem: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
   },
 });
